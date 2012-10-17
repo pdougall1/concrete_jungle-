d348547adaa439b73e7d2699a08bc20dd6d7982d
pdougall1 <pdougall1@gmail.com>
2012-10-17 15:31:49 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ConcreteJungle::Application.config.secret_token = '09b06c9021ef422c4dd9de47538bcc8c7ada824da22f36b314c54a26d1c904cd5c593f1cdde00426cf0e4a69bd7dee0adfb4d7b395638f8b2dc5d8b034778cb9'
